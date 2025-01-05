dbadeb5f5ec533f0f2c66a88b2492bc4735c902c
reocoker85 <139924408+reocoker85@users.noreply.github.com>
2025-01-06 00:53:58 +0300

    Update registry.tf

--- registry.tf
+++ registry.tf
@@ -2,7 +2,3 @@ resource "yandex_container_registry" "diplom_registry" {
   name      = var.registry_name
   folder_id = var.folder_id
 }
-
-output "registry_url" {
-  value = yandex_container_registry.diplom_registry.id
-}
