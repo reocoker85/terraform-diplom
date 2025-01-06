39f3f54d2085b8993c05a1372607205c05990f98
KIG <reocoker1985@yandex.ru>
2025-01-06 12:56:16 +0300

    test

--- network.tf
+++ network.tf
@@ -2,6 +2,13 @@ resource "yandex_vpc_network" "my_vpc" {
   name = var.vpc_name
 }
 
+resource "yandex_vpc_subnet" "public" {
+  name           = var.subnet_public_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.my_vpc.id
+  v4_cidr_blocks = var.public_cidr
+}
+
 resource "yandex_vpc_subnet" "my_subnet" {
   count          = length(var.zones)
   name           = "${var.subnet_name}-${var.zones[count.index]}"
